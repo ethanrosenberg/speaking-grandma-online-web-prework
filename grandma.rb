39c5b0b93c2c18233692f688ce8f27fd7616e9e7
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-02 08:29:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@ require 'pry'
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
+  if phrase.upcase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   if !phrase.upcase
     #binding.pry
     return "HUH?! SPEAK UP, SONNY!"
