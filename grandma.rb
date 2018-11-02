fc2d2687b0dfa126b2cb641b3b66fc53f2b26e83
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-02 08:31:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,11 +5,12 @@ require 'pry'
 def speak_to_grandma(phrase)
   if phrase.upcase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase
+    return "NO, NOT SINCE 1938!"
   elsif !phrase.upcase
     #binding.pry
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    return "NO, NOT SINCE 1938!"
+ 
   elsif phrase.upcase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   
